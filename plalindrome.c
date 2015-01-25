4ecaf928d7c05f4e53b0594807a02a93e6207e5a
isaaclzq <isaac.lzy@gmail.com>
2015-01-25 12:02:21 -0800

    will be using malloc to solve the problem

---
+++ plalindrome.c
@@ -0,0 +1,17 @@
+/*
+ * This program will verify if a number is a plalindrom number. For example, 
+ * 123321 is a plalindrome number. In this case, I will regard single integer 
+ * such as 1, 2, 3... as plalindrom number. 
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int plalindrom_test(unsigned int num){
+	int* store = 
+}
+
+int main(void){
+
+	return EXIT_SUCCESS;
+}
