107d40e9baaac502fc720af995c52e03ef12b48e
isaaclzq <isaac.lzy@gmail.com>
2015-01-25 17:42:05 -0800

    done for plalindrome

--- plalindrome.c
+++ plalindrome.c
@@ -6,12 +6,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int plalindrom_test(unsigned int num){
-	int* store = 
+int plalindrome_test(char* num, int size){
+	if (size == 1){
+		return 1;
+	} else if (size == 0){
+		return 1;
+	} else if (*num == *(num+size-1)){
+		return plalindrome_test(num+1, size-2);
+	} else {
+		return 0;
+	}
+	return 0;
 }
-
+	
 int main(void){
-
+	char* test1 = "abccba";
+	char* test2 = "121212";
+	char* test3 = "123421";
+	printf("test1 is %d\n", plalindrome_test(test1, 6));
+	printf("test2 is %d\n", plalindrome_test(test2, 6));
+	printf("test3 is %d\n", plalindrome_test(test3, 6));
 	return EXIT_SUCCESS;
 }
